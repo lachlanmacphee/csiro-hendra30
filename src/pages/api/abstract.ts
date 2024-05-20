a56e93aa7d87a1e7df47f435cb0eb621f96d6ff3
Lachlan MacPhee <lachlan.macphee@mantelgroup.com.au>
2024-05-21 00:41:54 +1000

    finish abstract submission api

--- src/pages/api/abstract.ts
+++ src/pages/api/abstract.ts
@@ -1,4 +1,18 @@
+import credentials from "../../../hendra30-ae97703a6cae.json";
 import type { APIRoute } from "astro";
+import { google } from "googleapis";
+
+// Initialise the Google Sheets API client
+const sheets = google.sheets({
+  version: "v4",
+  auth: new google.auth.GoogleAuth({
+    credentials,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  }),
+});
+
+const spreadsheetId = "1JM8CZmKATTGBeBBckWz0ecGjgcIdvM75qTguvdmD1-U";
+const sheetName = "Abstracts";
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
@@ -33,9 +47,31 @@ export const POST: APIRoute = async ({ request }) => {
     );
   }
 
+  await sheets.spreadsheets.values.append({
+    spreadsheetId,
+    range: `${sheetName}!A:Z`,
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [
+        [
+          firstName,
+          surname,
+          email,
+          abstractTitle,
+          abstractAuthors,
+          abstractAffiliations,
+          abstractBody,
+          abstractType,
+          abstractTopic,
+          abstractStudentLmic,
+        ],
+      ],
+    },
+  });
+
   return new Response(
     JSON.stringify({
-      message: "Success!",
+      message: "Your abstract was added!",
     }),
     { status: 200 },
   );
