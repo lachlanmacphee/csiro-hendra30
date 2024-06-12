bdf80487feea429cf701e02921f9abe6b784493d
Lachlan MacPhee <github@lachlanmacphee.com>
2024-06-12 12:10:01 +1000

    add sitemap and robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  site: "https://hendra30.com",
+  integrations: [tailwind(), preact(), sitemap()],
   output: "server",
   adapter: vercel(),
 });
